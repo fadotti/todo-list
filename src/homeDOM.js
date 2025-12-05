51d6685f34dcd4678b07cc3c4646ad04959072e7
Federico Dotti <fede.dotti.94@gmail.com>
2025-12-05 02:18:07 +0000

    Render homepage via homeDOM function

--- src/homeDOM.js
+++ src/homeDOM.js
@@ -22,13 +22,53 @@ function renderHome() {
   image.src = plus;
   const span = document.createElement("span");
   span.textContent = "Add a Project";
-
   document.querySelector("#nav > button").append(image.cloneNode(true), span.cloneNode(true));
+
+  div.removeAttribute('id');
+  document.querySelector("#home-container").appendChild(div.cloneNode(true));
+
+  div.id = "project-banner";
+  div.textContent = "My Projects";
+  document.querySelector("#home-container > div:nth-child(2)").appendChild(div.cloneNode(true));
+
+  Object.keys(localStorage).forEach(key => {
+    const projectObject = JSON.parse(localStorage.getItem(key));
+    Object.defineProperty(projectObject, 'numberOfTasks', {
+      get: function() {
+        return this.taskList.length
+      }
+    })
+    div.className = "project-card";
+    div.textContent = '';
+    div.removeAttribute('id');
+    document.querySelector("#home-container > div:nth-child(2)").appendChild(div.cloneNode(true));
+
+    div.removeAttribute('class');
+    document.querySelector("div.project-card").append(div.cloneNode(true), div.cloneNode(true), div.cloneNode(true));
+
+    span.textContent = projectObject.title;
+    document.querySelector("div.project-card > div:first-child").append(span.cloneNode(true));
+
+    div.textContent = `Created: ${projectObject.created}`;
+    document.querySelector("div.project-card > div:nth-child(2)").append(div.cloneNode(true));
+
+    div.textContent = `Last Edited: ${projectObject.created}`;
+    document.querySelector("div.project-card > div:nth-child(2)").append(div.cloneNode(true));
+
+    div.textContent = `Number of Tasks: ${projectObject.numberOfTasks}`;
+    document.querySelector("div.project-card > div:nth-child(2)").append(div.cloneNode(true));
+
+    document.querySelector("div.project-card > div:nth-child(3)").append(button.cloneNode(true), button.cloneNode(true), button.cloneNode(true));
+
+    image.src = openFolder;
+    document.querySelector("div.project-card > div:nth-child(3) > button:first-child").append(image.cloneNode(true));
+
+    image.src = pencil;
+    document.querySelector("div.project-card > div:nth-child(3) > button:nth-child(2)").append(image.cloneNode(true));
+
+    image.src = trashCan;
+    document.querySelector("div.project-card > div:nth-child(3) > button:nth-child(3)").append(image.cloneNode(true));
+  })
 }
 
 export {renderHome}
-
-
-
-
-
